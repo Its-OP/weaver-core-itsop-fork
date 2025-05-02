1af857a9edc255a8cd9928887528141fa5ad3fe8
Oleh Prostakov <110679395+Its-OP@users.noreply.github.com>
2025-05-02 13:50:09 +0300

    fix logs

--- weaver/train.py
+++ weaver/train.py
@@ -20,10 +20,7 @@ from weaver.utils.import_tools import import_module
 
 import logging, torch._logging as tl
 
-tl.set_logs(
-    dynamo="debug",   # equivalent to +dynamo
-    recompiles=True   # turn on the artifact
-)
+tl.set_logs(dynamo=logging.DEBUG, recompiles=True)
 
 import warnings
 warnings.filterwarnings('ignore')  # Ignore all warnings to prevent compiler errors
