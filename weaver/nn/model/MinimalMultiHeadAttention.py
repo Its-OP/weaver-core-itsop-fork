c22dd0f56da4d234fd72cecea5a1ba2492499b04
Oleh Prostakov <110679395+Its-OP@users.noreply.github.com>
2025-05-04 23:38:41 +0300

    Revert "fix init"
    This reverts commit 5ed62a0647b208c8902a79e77d28a808fb3474c0.

--- weaver/nn/model/MinimalMultiHeadAttention.py
+++ weaver/nn/model/MinimalMultiHeadAttention.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class MinimalMultiheadAttention(nn.Module):
+class MinimalMultiHeadAttention(nn.Module):
     def __init__(
             self,
             embed_dim: int,
