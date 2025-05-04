5ed62a0647b208c8902a79e77d28a808fb3474c0
Oleh Prostakov <110679395+Its-OP@users.noreply.github.com>
2025-05-04 23:27:48 +0300

    fix init

--- weaver/nn/model/MinimalMultiHeadAttention.py
+++ weaver/nn/model/MinimalMultiHeadAttention.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class MinimalMultiHeadAttention(nn.Module):
+class MinimalMultiheadAttention(nn.Module):
     def __init__(
             self,
             embed_dim: int,
